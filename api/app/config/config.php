3965d6792125fd454e9ef448f0820c685e3ba230
Marcos Guerreros <guerrerosocom@gmail.com>
2024-02-28 12:07:11 +0100

    Update Constantes App

--- api/app/config/config.php
+++ api/app/config/config.php
@@ -6,7 +6,21 @@ define('RUTA_URL', 'http://localhost:8080/LogroFilmApi/api');
 
 define('DB_HOST', 'localhost'); //tu servidor de BD.
 define('DB_USUARIO', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 define('DB_NOMBRE', 'taskforce'); // Tu base de datos
 
 define('SECRET_KEY', 'taskforce');
+
+define('ACCION_LOGIN','HA INICIADO SESION');
+define('ACCION_AGREGAR_USUARIO','HA AGREGADO UN USUARIO');
+define('ACCION_MODIFICAR_USUARIO','HA MODIFICADO UN USUARIO');
+define('ACCION_BORRAR_USUARIO','HA BORRADO UN USUARIO');
+
+
+define('ACCION_AGREGAR_TAREA','HA AGREGADO UNA TAREA');
+define('ACCION_MODIFICAR_TAREA','HA MODIFICADO UNA TAREA');
+define('ACCION_BORRAR_TAREA','HA BORRADO UNA TAREA');
+
+define('ACCION_AGREGAR_PROYECTO','HA AGREGADO UN PROYECTO');
+define('ACCION_MODIFICAR_PROYECTO','HA MODIFICADO UN PROYECTO');
+define('ACCION_BORRAR_PROYECTO','HA BORRADO UN PROYECTO');
